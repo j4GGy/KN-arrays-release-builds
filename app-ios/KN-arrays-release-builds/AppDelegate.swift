01c937d0c90398aecc2a66d84656a115cf805604
j4GGy <jagie4ever@hotmail.com>
2020-07-16 11:06:14 +0200

    iOS project

---
+++ app-ios/KN-arrays-release-builds/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  KN-arrays-release-builds
+//
+//  Created by TesMath on 16.07.20.
+//  Copyright © 2020 TesMath. All rights reserved.
+//
+
+import UIKit
+import KNArrays
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        doTests()
+        
+        return true
+    }
+    
+    func doTests() {
+        let array1 = ArraysConstructor(int1: 1, int2: 2)
+        array1.log()
+        
+        print("--------------------------------")
+        
+        let array2 = ArraysDefault(int1: 1, int2: 2)
+        array2.log()
+        
+        print("--------------------------------")
+        array1.set(int1: 3, int2: 4)
+        array1.log()
+        
+        print("--------------------------------")
+        array2.set(int1: 3, int2: 4)
+        array2.log()
+        
+    }
+}
+
---
+++ app-ios/KN-arrays-release-builds/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  KN-arrays-release-builds
+//
+//  Created by TesMath on 16.07.20.
+//  Copyright © 2020 TesMath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
